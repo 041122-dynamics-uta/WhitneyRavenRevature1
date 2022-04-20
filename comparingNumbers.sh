b206fa01e10044ccf25c7593a5fb9738773cc47c
WhitneyRavenRevature <103517973+WhitneyRavenRevature@users.noreply.github.com>
2022-04-19 21:57:10 -0400

    Add files via upload

---
+++ comparingNumbers.sh
@@ -0,0 +1,16 @@
+read X
+read Y
+
+if (( $X > $Y));
+then
+    echo "X is greater than Y"
+
+elif (( $X == $Y ));
+then
+    echo "X is equal to Y"
+
+elif (( $X < $Y ));
+then
+    echo "X is less than Y"
+
+fi
